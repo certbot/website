17d96c0ff55e58f901419523730b931424319dbb
Noah Swartz <swartzcr@gmail.com>
2016-03-25 17:40:19 -0700

    add centos and gentoo and bsd install

--- instruction-generator.js
+++ instruction-generator.js
@@ -9,7 +9,7 @@ input_map = {
      },
      "Debian 8 (jessie)" : {
          os: "debian",
-         os_version: 7,
+         os_version: 8,
      },
     "Debian testing/unstable" : {
          os: "debian",
@@ -20,15 +20,15 @@ input_map = {
         os_version: 0,
     },
     "Ubuntu 16.04 (xenial)" : {
-        os: "debian",
+        os: "ubuntu",
         os_version: 16.04
     },
     "Ubuntu 14.04 (trusty)" : {
-        os: "debian",
+        os: "ubuntu",
         os_version: 14.04
     },
     "Ubuntu (other)" : {
-        os: debian,
+        os: "ububtu",
         os_version: 0
     },
     "Gentoo" : {
@@ -83,21 +83,59 @@ print_install_instructions = function() {
     if (input.os == "debian" or input.os == "ubuntu") {
         return print_debian_install_instructions()
     }
-    if (input.os = "python"){
+    if (input.os == "python"){
         return print_pip_install_instructions()
+    }
+    if (input.os == "gentoo"){
+        return print_gentoo_install_instructions()
+    }
+    if (input.os == "bsd"){
+        return print_bsd_install_instructions()
+    }
+    if (input.os == "rhel" or input.os == "centos" or input.os == "fedora"){
+        return print_rhel_install_instructions()
     }
     print_cbauto_instructions()
 }
 
+print_gentoo_install_instructions = function() {
+    package_name = "certbot"
+    if (input.webserver == "apache") {
+        package_name = "certbot-apache"
+        command = "certbot-apache"
+    }
+    return iprint("emerge " + package_name)
+}
+
+print_bsd_install_instructions = function() {
+    return iprint("pkg install py27-letsencrypt"
+}
+
+print_rhel_install_instructions = function() {
+    // from: https://digitz.org/blog/lets-encrypt-ssl-centos-7-setup/
+    if (input.os == "centos") {
+        setup = "yum install epel-release\n"
+        if (input.os_version < 7) {
+            setup += "rpm -ivh https://rhel6.iuscommunity.org/ius-release.rpm\n"
+            setup += "yum --enablerepo=ius install git python27 python27-devel python27-pip python27-setuptools python27-virtualenv -y\n"
+        }
+        setup += "yum install git\n"
+        return print_cbauto_instructions(setup)
+    }
+    else if (input.os == "rhel") {
+        return print_cbauto_instructions()
+    }
+}
+
 print_debian_install_instructions = function() {
     backport = "";
-    if (input.os_version == "debian" and input.os_version <= 7) {
+    if (input.os == "debian" and input.os_version <= 7) {
         return print_cbauto_instructions();
     }
-    else if (input.os_version == "ubuntu" and input.os_version <= 15.10 {
+    else if (input.os == "ubuntu" and input.os_version <= 15.10 {
         return print_cbauto_instructions();
     }
-    else (input.os_version == 8) {
+    else if (input.os == "debian" and input.os_version == 8) {
         iprint(strings.jessie_backports_instructions);
         backport = " -t jessie-backports "
     }
@@ -108,8 +146,8 @@ debian_packages = function() {
     return strings["debian"][input.webserver];
 }
 
-print_cbauto_instructions = function() {
-    iprint(strings.cbauto_install);
+print_cbauto_instructions = function(inp="") {
+    iprint(inp + strings.cbauto_install);
     command = strings.cbauto_cmd;
 }
 
