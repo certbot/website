c9d241b2ebb6bd127431d17e817225a4126c2430
vivian <vivian@eff.org>
2016-04-12 17:34:47 -0700

    Combine os_map and other input lists into a single json blob.

--- _scripts/instruction-widget.js
+++ _scripts/instruction-widget.js
@@ -1,11 +1,13 @@
 var $ = require('jquery');
+var _ = require('lodash');
 var generator = require('./instruction-generator');
+var inputs = require('./data/instruction-inputs.json')
 
 InstructionWidget = {
   init: function() {
-    this.setup_select("os-select", Object.keys(generator.os_map))
-    this.setup_select("server-select", generator.known_webservers)
-    this.setup_select("usecase-select", generator.usecases)
+    this.setup_select("os-select", _.keys(inputs.operating_systems))
+    this.setup_select("server-select", _.keys(inputs.webservers))
+    this.setup_select("usecase-select", _.keys(inputs.use_cases))
     this.get_instructions()
   },
 
@@ -15,13 +17,13 @@ InstructionWidget = {
 
   get_instructions: function() {
     var text = this.get_selected_text("os-select");
-    var selected_os = generator.os_map[text]
+    var selected_os = inputs.operating_systems[text]
 
     input = {
-      os : selected_os.os,
-      os_version : selected_os.os_version,
-      webserver : this.get_selected_text("server-select"),
-      usecase : this.get_selected_text("usecase-select"),
+      os: selected_os.distro,
+      os_version: selected_os.version,
+      webserver: this.get_selected_text("server-select"),
+      usecase: this.get_selected_text("usecase-select"),
     }
 
     try {
@@ -38,7 +40,7 @@ InstructionWidget = {
   setup_select: function(select_id, options) {
     var select = document.getElementById(select_id)
     options.forEach(function (item, idx, arr) {
-        select.options.add(new Option(item))
+      select.options.add(new Option(item))
     })
     select.onchange = function() {
       InstructionWidget.get_instructions()
