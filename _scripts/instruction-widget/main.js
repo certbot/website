b8216eee9db9c6c5b50c0cebf903c25482d2d841
vivian <vivian@eff.org>
2016-04-21 20:00:37 -0700

    Rename instruction widget data files.

--- _scripts/instruction-widget/main.js
+++ _scripts/instruction-widget/main.js
@@ -1,6 +1,6 @@
 var $ = require('jquery');
 var Instructions = require('./instructions');
-var inputs = require('./data/instruction-inputs.json');
+var inputs = require('./data/inputs.json');
 
 /**
  * Renders and controls a widget that allows users to get installation
