642f9077bea1ba659abbfdd2c5d474e50ec31a35
vivian <vivian@eff.org>
2016-04-26 16:50:11 -0700

    Read build/watch environment from environment variable or command line flag.

--- _gulp/tasks/css.js
+++ _gulp/tasks/css.js
@@ -1,18 +1,16 @@
 var gulp = require('gulp'),
-    gulpif = require('gulp-if'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     autoprefixer = require('gulp-autoprefixer'),
-    globbing = require('gulp-css-globbing')
+    globbing = require('gulp-css-globbing'),
+    env = require('gulp-environments'),
     browserSync = require('browser-sync').create();
 
 var config = require('../config');
 
-var isProduction = false;
-
 gulp.task('css', (done) => {
   return gulp.src(config.css.src)
-    .pipe(gulpif(!isProduction, sourcemaps.init()))
+    .pipe(env.development(sourcemaps.init()))
     .pipe(globbing({
         extensions: ['.scss']
     }))
@@ -20,7 +18,7 @@ gulp.task('css', (done) => {
     .pipe(autoprefixer({
         browsers: ['last 2 version']
     }))
-    .pipe(gulpif(!isProduction, sourcemaps.write('.')))
+    .pipe(env.development(sourcemaps.write('.')))
     .pipe(gulp.dest(config.css.dest))
     .pipe(browserSync.stream({match: '**/*.css'}));
 });
