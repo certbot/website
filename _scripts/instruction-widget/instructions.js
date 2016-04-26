49b10b67b9638e61fe0bb217a866172e8aaffbbe
vivian <vivian@eff.org>
2016-04-25 22:33:14 -0700

    Remove unnecessary line wrap.

--- _scripts/instruction-widget/instructions.js
+++ _scripts/instruction-widget/instructions.js
@@ -17,11 +17,9 @@ module.exports = function() {
   };
 
   get_partials = function(input) {
-    // Add user inputs to the context:
-    // distro, version and webserver.
+    // Add user inputs to the context: distro, version and webserver.
     _.extend(context, input);
-    // Allow templates to render instruction blocks based
-    // on a user's webserver.
+    // Allow templates to render instruction blocks based on a user's webserver.
     context[input.webserver] = true;
 
     // Generate automated and advanced instruction sets.
