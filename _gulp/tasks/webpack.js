775a23e029f1fd3c17ca2ddf605939036f41e9b6
vivian <vivian@eff.org>
2016-04-27 13:37:21 -0700

    Build js sourcemaps in dev, minify in production.

--- _gulp/tasks/webpack.js
+++ _gulp/tasks/webpack.js
@@ -1,25 +1,33 @@
 var gulp = require('gulp'),
+    env = require('gulp-environments'),
     webpack = require('gulp-webpack');
 
 var config = require('../config');
 
+config.webpack = {
+  entry: {
+    main: './_scripts/main',
+    instructions: './_scripts/instruction-widget/main',
+  },
+  output: {
+    filename: '[name].js',
+  },
+  devtool: env.development() ? "source-map" : "",
+  module: {
+    loaders: [
+      {test: /\.json$/, loader: 'json'},
+      {test: /\.html$/, loader: 'mustache?noShortcut'}
+    ]
+  },
+  plugins: env.production() ? [
+    new webpack.webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
+    })
+  ] : []
+}
+
 gulp.task('js', (done) => {
   return gulp.src(config.js.src)
-    .pipe(webpack({
-      entry: {
-        main: './_scripts/main',
-        instructions: './_scripts/instruction-widget/main',
-      },
-      output: {
-        filename: '[name].js',
-      },
-      devtool: "source-map",
-      module: {
-        loaders: [
-          {test: /\.json$/, loader: 'json'},
-          {test: /\.html$/, loader: 'mustache?noShortcut'}
-        ]
-      }
-    }))
+    .pipe(webpack(config.webpack))
     .pipe(gulp.dest(config.js.dest));
 });
