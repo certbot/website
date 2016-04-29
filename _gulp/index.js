3f6fdf71c02218dbeab4052e3e18315e59f6905f
mark burdett <mfb@eff.org>
2016-04-28 17:21:04 -0700

    add virtual environment setup and gulp task for building sphinx docs

--- _gulp/index.js
+++ _gulp/index.js
@@ -6,6 +6,6 @@ tasks.forEach(function(task) {
   require('./tasks/' + task);
 });
 
-gulp.task('watch', ['json-instructions', 'css', 'js', 'jekyll:watch', 'serve']);
-gulp.task('build', ['json-instructions', 'css', 'js', 'jekyll:build']);
+gulp.task('watch', ['json-instructions', 'css', 'js', 'jekyll:watch', 'docs:install', 'serve']);
+gulp.task('build', ['json-instructions', 'css', 'js', 'jekyll:build', 'docs:install']);
 
