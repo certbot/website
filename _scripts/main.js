1d1f761ccff880a288a4d9d4675db4389d364c0a
mark burdett <mfb@eff.org>
2016-05-09 16:25:02 -0700

    add license fixes #40

--- _scripts/main.js
+++ _scripts/main.js
@@ -1,5 +1,23 @@
 // js loaded on every page goes here.
 
+/**
+ * Client-side code for the Certbot website.
+ * Copyright (C) 2016  Electronic Frontier Foundation
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 (function() {
 
   var hamburger = document.getElementById('hamburger');
