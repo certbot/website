86ffe43c06d67b18a0b4d7f61a403be70837e745
Peter Eckersley <pde@eff.org>
2016-05-12 10:38:36 -0700

    Final tagline, and associated CSS tweaks.

--- _scripts/instruction-widget/instructions.js
+++ _scripts/instruction-widget/instructions.js
@@ -39,7 +39,7 @@ module.exports = function() {
         (input.version == null) ||
         (input.webserver == null)) {
       return "To get instructions for Certbot, choose your webserver " +
-        "and server operating system from the dropdown menus above.";
+        'and server operating system from the dropdown menus above. You can then pick "advanced" if you want less automation and more control.';
     }
     var partials = get_partials(input);
     var template = require("./templates/instructions.html")
