ef545fbcd1a8ae9e18008e6c601e16160b9b38d4
vivian <vivian@eff.org>
2017-07-06 09:08:05 -0700

    Specify branch for git push

--- push-build.sh
+++ push-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-set -e
 
+rev=$(git rev-parse --short HEAD)
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
@@ -11,11 +11,12 @@ git config user.name "Travis CI"
 git config user.email "builds@travis-ci.com"
 
 git remote add upstream "git@github.com:vbrown608/certbot-builds.git"
-git fetch upstream
 git checkout -B $TRAVIS_BRANCH
+git fetch upstream $TRAVIS_BRANCH
+git reset upstream/$TRAVIS_BRANCH
 
 touch .
 
 git add -A
 git commit -m "Build website at ${rev}"
-git push -q upstream
+git push -q upstream $TRAVIS_BRANCH
