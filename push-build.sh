a3d125c7e0c28eac90a49517acfb594ef348c5f7
vivian <vivian@eff.org>
2017-07-05 17:50:24 -0700

    Use encrypted ssh key to push

--- push-build.sh
+++ push-build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
 
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
 cd _site
 git init
 git config user.name "Travis CI"
 git config user.email "builds@travis-ci.com"
 
-git remote add upstream "https://$GIT_TOKEN@github.com/vbrown608/certbot-builds.git"
+git remote add upstream "git@github.com:vbrown608/certbot-builds.git"
 git fetch upstream
 git checkout -B $TRAVIS_BRANCH
 
