dbf19f71dc696af231120db6bc139b02c1d6ed64
vivian <vivian@eff.org>
2017-07-07 09:51:46 -0700

    Better accomodate builds triggered by PRs
    PR builds are different from builds on a branch because they build
    the result of a hypothetical merge with master. Committing the result of that
    build to the source branch's analog in the website-builds repo is a little
    weird, but seems least likely to make a mess in the website-builds repo.
    
    This commit makes the following changes:
    - Commit PR builds to the source branch instead of master
    - Leave a helpful commit message referencing the original PR
    - Set HEAD to the source branch manually rather than relying on `git checkout`

--- push-build.sh
+++ push-build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-rev=$(git rev-parse --short HEAD)
+if [ "$TRAVIS_EVENT_TYPE" == "pull_request" ]
+then
+  BRANCH=$TRAVIS_PULL_REQUEST_BRANCH
+  BUILD="PR #${TRAVIS_PULL_REQUEST}"
+else
+  BRANCH=$TRAVIS_BRANCH
+  BUILD=$(git rev-parse --short HEAD)
+fi
+
 chmod 600 build_key
 eval `ssh-agent -s`
 ssh-add build_key
@@ -13,14 +21,18 @@ git remote add upstream "git@github.com:certbot/website-builds.git"
 
 # Branch from master to more easily compare across branches.
 git fetch upstream master
-git checkout -B $TRAVIS_BRANCH upstream/master
+git branch $BRANCH upstream/master
+# Manually set which branch HEAD points to to avoid changes to the
+# working tree caused by "git checkout".
+git symbolic-ref HEAD refs/heads/$BRANCH
+git reset
 
 # If this branch already exists upstream, use that instead.
-git fetch upstream $TRAVIS_BRANCH
-git reset upstream/$TRAVIS_BRANCH
+git fetch upstream $BRANCH
+git reset upstream/$BRANCH
 
 touch .
 
 git add -A
-git commit -m "Build website at ${rev}"
-git push -q upstream $TRAVIS_BRANCH
+git commit -m "Build website at ${BUILD}"
+git push -q upstream $BRANCH
