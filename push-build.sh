889977e0b169fb66c7b71c3fda4abb15ad9021de
vivian <vivian@eff.org>
2017-07-12 11:33:23 -0700

    Clearer branch logic for build script
    When previously committing the build, the choice between cutting a new branch from master or pulling an existing feature branch was implicit, based on whether the pull failed. This commit adds and explicit conditional.

--- push-build.sh
+++ push-build.sh
@@ -13,17 +13,21 @@ git config user.name "Travis CI"
 git config user.email "builds@travis-ci.com"
 git remote add upstream "git@github.com:certbot/website-builds.git"
 
-# Branch from master to more easily compare across branches.
-git fetch upstream master
-git branch $BRANCH upstream/master
-# Manually set which branch HEAD points to to avoid changes to the
-# working tree caused by "git checkout".
-git symbolic-ref HEAD refs/heads/$BRANCH
-git reset
-
-# If this branch already exists upstream, use that instead.
 git fetch upstream $BRANCH
-git reset upstream/$BRANCH
+branch_exists=$?
+if [ $branch_exists == 0 ]
+then
+  # Manually set which branch HEAD points to to avoid changes to the
+  # working tree caused by "git checkout".
+  git symbolic-ref HEAD refs/heads/$BRANCH
+  git reset upstream/$BRANCH
+else
+  # Branch from master to more easily compare across branches.
+  git fetch upstream master
+  git branch $BRANCH upstream/master
+  git symbolic-ref HEAD refs/heads/$BRANCH
+  git reset
+fi
 
 touch .
 
