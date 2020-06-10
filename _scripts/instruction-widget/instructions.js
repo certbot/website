fd1c866005fd35d1b8f94c2494cb88a4d001b08d
Brad Warren <bmw@users.noreply.github.com>
2020-06-10 16:22:37 -0700

    Remove reference to the advanced tab. (#592)

--- _scripts/instruction-widget/instructions.js
+++ _scripts/instruction-widget/instructions.js
@@ -39,9 +39,7 @@ module.exports = function() {
         (input.version == null) ||
         (input.webserver == null)) {
       return 'To get instructions for Certbot, choose your server software ' +
-        'and the system it is running on from the dropdown menus above. ' +
-        'You can then pick "advanced" if you want less automation and more ' +
-        'control.';
+        'and the system it is running on from the dropdown menus above.';
     }
     var partials = get_partials(input);
     var template = require("./templates/instructions.html")
