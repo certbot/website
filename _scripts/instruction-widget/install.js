certbot/website

commit 531c3dd3889b0b5e0e3a6da6dfd149b34ef82bd0

Have CentOS/RHEL 8 users use EPEL 8 instead of certbot-auto (#581)
* Use EPEL 8 for CentOS/RHEL 8.

* Simplify/generalize EPEL instructions

* Explicitly mention the extra repositories.

_scripts/instruction-widget/install.js
@@ -92,8 +92,8 @@ module.exports = function(context) {
   centos_install = function() {
     template = "centos";
 
-    // Certbot only has packages available for RHEL 7 based systems.
-    if (context.version != 7) {
+    // Certbot only has packages available for RHEL 7+ based systems.
+    if (context.version < 7) {
       context.base_command = "/usr/local/bin/certbot-auto";
       // RHEL/CentOS 6 32 bits distros are not supported by certbot-auto.
       if (context.version == 6) {
@@ -107,17 +107,25 @@ module.exports = function(context) {
     } else {
       context.need_epel = true;
       context.base_command = "certbot";
-      context.install_command = "sudo yum install";
       context.package = "certbot";
       context.packaged = true;
 
+      if (context.version == 7) {
+        context.install_command = "sudo yum install";
+        python_prefix = "python2-"
+      } else {
+        context.install_command = "sudo dnf install";
+        python_prefix = "python3-"
+      }
+
+
       if (context.webserver == "apache") {
-        context.package += " python2-certbot-apache";
+        context.package += " " + python_prefix + "certbot-apache";
       } else if (context.webserver == "nginx") {
-        context.package += " python2-certbot-nginx";
+        context.package += " " + python_prefix + "certbot-nginx";
       }
       context.dns_plugins = true;
-      context.dns_package_prefix = "python2-certbot-dns";
+      context.dns_package_prefix = python_prefix + "certbot-dns";
     }
   }
 

_scripts/instruction-widget/templates/install/centos.html
@@ -17,24 +17,16 @@
     Enable EPEL repo
     <p>
     You'll need to enable the EPEL (Extra Packages for Enterprise Linux)
-    repository.<br>
+    repository and make sure you follow all instructions for your system,
+    including enabling any other recommended repositories that may be
+    required.<br>
     Follow these instructions at
-    <a href='https://fedoraproject.org/wiki/EPEL#How_can_I_use_these_extra_packages.3F'>
+    <a href='https://fedoraproject.org/wiki/EPEL#Quickstart'>
       the Fedora wiki to enable EPEL</a>.
     </p>
     <p class="centered">
-    <a class="link-button" href='https://fedoraproject.org/wiki/EPEL#How_can_I_use_these_extra_packages.3F'>
+    <a class="link-button" href='https://fedoraproject.org/wiki/EPEL#Quickstart'>
       enable EPEL</a>
-    </p>
-</li>
-<li>
-    Enable the optional channel
-    <p>
-    If you're using RHEL or Oracle Linux, you'll also need to enable the optional channel.
-    On EC2, RHEL users can enable the optional channel by running the following command,
-    substituting your EC2 region for REGION in the command:
-    <pre class="no-before"><ol><li>yum -y install yum-utils</li>
-      <li>yum-config-manager --enable rhui-REGION-rhel-server-extras rhui-REGION-rhel-server-optional</li></ol></pre>
     </p>
 </li>
 
@@ -43,22 +35,6 @@
 {{/packaged}}
 
 {{^packaged}}
-{{#need_epel}}
-<li>
-    Enable EPEL repo
-    <p>
-    You'll need to enable the EPEL (Extra Packages for Enterprise Linux)
-    repository to download Certbot's dependecies.<br>
-    Follow these instructions at
-    <a href='https://fedoraproject.org/wiki/EPEL#How_can_I_use_these_extra_packages.3F'>
-      the Fedora wiki to enable EPEL</a>.
-    </p>
-    <p class="centered">
-    <a class="link-button" href='https://fedoraproject.org/wiki/EPEL#How_can_I_use_these_extra_packages.3F'>
-      enable EPEL</a>
-    </p>
-</li>
-{{/need_epel}}
 {{>auto}}
 {{/packaged}}
 

commit b9b22a5ea3e3d0cd778912f40b47814a95f495c9

fix RHEL 8 instructions (#609)
As reported at https://community.letsencrypt.org/t/selinux-should-the-log-files-be-var-log-t-or-cron-log-t/128552/2, our renewal instructions for CentOS/RHEL 8 don't work. We initially fixed this in https://github.com/certbot/website/pull/528 but broke it in https://github.com/certbot/website/pull/581. This PR fixes them again.

_scripts/instruction-widget/install.js
@@ -88,21 +88,17 @@ module.exports = function(context) {
   centos_install = function() {
     template = "centos";
 
-    // Certbot only has packages available for RHEL 7+ based systems.
+    // Certbot only has packages available for RHEL 7+ based systems and RHEL 6
+    // is the oldest version we support.
     if (context.version < 7) {
       context.base_command = "/usr/local/bin/certbot-auto";
-      // RHEL/CentOS 6 32 bits distros are not supported by certbot-auto.
-      if (context.version == 6) {
-        context.deprecated_32bits = true
-      } else {
-        context.deprecated_32bits = false
-        context.python_name = "python3";
-      }
+      context.deprecated_32bits = true
       context.need_epel = false;
       context.packaged = false;
     } else {
-      context.need_epel = true;
       context.base_command = "certbot";
+      context.deprecated_32bits = false;
+      context.need_epel = true;
       context.package = "certbot";
       context.packaged = true;
 
@@ -110,7 +106,9 @@ module.exports = function(context) {
         context.install_command = "sudo yum install";
         python_prefix = "python2-"
       } else {
+        // In this case we're on RHEL 8+
         context.install_command = "sudo dnf install";
+        context.python_name = "python3";
         python_prefix = "python3-"
       }
 