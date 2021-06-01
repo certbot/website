0e7ef3f2006ff2c1d5e12c67381f61fd1b61623d
Erica Portnoy <ebportnoy@gmail.com>
2021-06-01 10:07:14 -0700

    Turn on certonly for haproxy

--- _scripts/instruction-widget/get-started.js
+++ _scripts/instruction-widget/get-started.js
@@ -43,6 +43,7 @@ module.exports = function(context) {
         context.officially = "officially ";
         context.imperative = "you should probably"
         template = "haproxy";
+        context.certonly = true;
     }
 
     plesk_getting_started = function() {
