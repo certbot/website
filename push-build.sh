c636e767af84286affd0cbc634a8173fa722822a
Brad Warren <bmw@users.noreply.github.com>
2021-08-31 16:38:53 -0700

    Switch to GitHub Actions (#730)
    Fixes https://github.com/certbot/website/issues/487 and Travis not running because [it's no longer free for open source projects](https://travis-ci.community/t/org-com-migration-unexpectedly-comes-with-a-plan-change-for-oss-what-exactly-is-the-new-deal/10567).
    
    This GH Action setup should essentially be the same as our Travis setup except the SSH key being used is one I created for the GH account https://github.com/certbotbot and notifications about failed builds are sent directly to Mattermost.

--- push-build.sh
+++ push-build.sh
@@ -1,20 +1,18 @@
-#!/bin/bash -e
+#!/bin/bash
+set -euo pipefail
 
-BRANCH=$TRAVIS_BRANCH
+BRANCH=$(echo "${GITHUB_REF}" | sed -E 's!refs/(heads|tags)/!!g')
 BUILD=$(git rev-parse --short HEAD)
 
-if [ $TRAVIS == "true" ]
-then
-  openssl aes-256-cbc -K $encrypted_e5009b71da55_key -iv $encrypted_e5009b71da55_iv -in build_key.enc -out build_key -d
-fi
+echo "$CERTBOTBOT_SSH_KEY" > build_key
 chmod 600 build_key
 eval `ssh-agent -s`
 ssh-add build_key
 
 cd _site
 git init
-git config user.name "Travis CI"
-git config user.email "builds@travis-ci.com"
+git config user.name "Certbotbot"
+git config user.email "certbot-team@eff.org"
 git remote add upstream "git@github.com:certbot/website-builds.git"
 
 if git fetch upstream "$BRANCH"
